4eb727ff94050d2e2156e7100280a2a9adffaf63
arr90 <arr90rj@gmail.com>
2018-04-05 00:59:58 -0300

    add funcao para capturar alguns erros

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ const route = router.get('/', (request, response, next) => {//quando o usuario a
 app.use('/', route);
 
 server.listen(port); //pede pro servidor ficar ouvindo essa porta
+server.on('error', onError);
+server.on('listening', onListener);
+
 console.log("API rodando na porta "+ port);
 
 function normalizePort(val){
@@ -34,4 +37,30 @@ function normalizePort(val){
     }
 
     return false;
+}
+
+function onError(error){
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+    const bind = typeof port === 'string' ? 'Pipe '+port : 'Port '+port;
+
+    switch(error.code){
+        case 'EACCESS':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+function onListener(){
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe '+addr : 'port '+addr.port;
+    debug('Listening on ' + bind);
 }
