539363f8a9dbe19d391e43527b8717d00a3b9ecb
arr90 <arr90rj@gmail.com>
2018-04-06 03:10:13 -0300

    aplicando o modelo do mvc, criando schema para o banco onde terao as validacoes ja que o banco nao faz essas validacoes

---
+++ src/models/product.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const schema = new Schema({
+    //_id = criado automaticamente 'guid()'
+    title:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug:{
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    active:{
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags:[{
+        type: String,
+        required: true
+    }]
+});
+
+module.exports = mongoose.model('Product', schema);//exportacao do schema (nome do schema, obj schema)
