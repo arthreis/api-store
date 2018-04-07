1c8532667c2a06bf0b6aecf16f70a9bb33d5f3b4
arr90 <arr90rj@gmail.com>
2018-04-07 03:11:33 -0300

    add metodo para listar produtos usando filtro e campos que serao listados

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -8,5 +8,6 @@ const controller = require('../controllers/product-controller')
 router.post('/', controller.post);//quando o usuario acessar a url '/' ele caira na rota que criamos aqui
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
+router.get('/', controller.get);
 
 module.exports = router;
