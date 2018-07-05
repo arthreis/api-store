bd3926e98fe1482d1fedbe31ba1c8b38ac65e6c1
Arthur <arr90rj@gmail.com>
2018-07-05 01:11:02 -0300

    :heavy_plus_sign: - servico de envio de email

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY   = '68e583dc-d82b-426f-9040-1c416fd70095';
-global.EMAIL_TMPL = '<strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá <strong>{0}</strong>, seja bem vindo à NodeStore';
 
 module.exports = {
     connectionString: 'mongodb://ds030719.mlab.com:30719/areis-ndstore',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
