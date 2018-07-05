888cae64a8a9049906dbb5610836b3e49bd0833e
Arthur <arr90rj@gmail.com>
2018-07-05 00:13:54 -0300

    adicionado arquivo de configuração

---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY   = '';
+global.EMAIL_TMPL = '<strong>{0}</strong>';
+
+module.exports = {
+    connectionString: 'mongodb://ds030719.mlab.com:30719/areis-ndstore',
+    sendgridKey: 'TBD',
+    containerConnectionString: 'TBD'
+}
