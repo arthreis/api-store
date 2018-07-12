027d122f874d6db082ba2e6371934335134d0c52
Arthur <arr90rj@gmail.com>
2018-07-12 00:23:41 -0300

    :heavy_plus_sign: inclusao de servico para autenticacao

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/product-controller');
+const authService = require('../services/auth-service');
 
-const controller = require('../controllers/product-controller')
-
-router.post('/', controller.post);//quando o usuario acessar a url '/' ele caira na rota que criamos aqui
+router.post('/', authService.authorize, controller.post);//quando o usuario acessar a url '/' ele caira na rota que criamos aqui
 router.get('/:slug', controller.getBySlug);
 router.get('/admin/:id', controller.getById);
 router.get('/tags/:tag', controller.getByTag);
